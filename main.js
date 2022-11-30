64a4ea5affb1a89f345bb7fe61ee379711af4d97
Oxxra <87738373+Oxxra@users.noreply.github.com>
2022-11-29 17:52:34 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function start()
+{
+
+    document.getElementById("textbox").innerHTMl = "";
+    recognition.start();
+}
+
+recognition.onresult = function(event) {
+    console.log(event);
+    var Content = event.results[0][0].transcript;
+    console.log(Content);
+
+    document.getElementById("textbox").innerHTML = Content
+}
